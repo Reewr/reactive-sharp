4f7bf7baeaee3bc7d5d05c0c15d7776755a5d735
Alexander Imenes <alexander.imenes@gmail.com>
2024-12-14 20:56:26 +0100

    Make RendererNode fill screen

--- godot/src/Renderer.cs
+++ godot/src/Renderer.cs
@@ -11,12 +11,29 @@ public partial class Renderer : Godot.Control
 	{
 		instanceCount++;
 		ComponentRenderer = new ReactiveSharp.Renderer(RendererNode);
+		Position = new Godot.Vector2(0, 0);
+		MouseFilter = Godot.Control.MouseFilterEnum.Ignore;
 		RendererNode.Name = $"RenderRoot{instanceCount}";
+		RendererNode.Position = new Godot.Vector2(0, 0);
+		RendererNode.MouseFilter = Godot.Control.MouseFilterEnum.Ignore;
+	}
+
+	public override void _Ready()
+	{
+		GetViewport().SizeChanged += OnViewportSizeChanged;
+		OnViewportSizeChanged();
+	}
+
+	private void OnViewportSizeChanged()
+	{
+		RendererNode.Size = GetViewportRect().Size;
+		Size = GetViewportRect().Size;
 	}
 
 	public override void _EnterTree()
 	{
 		AddChild(RendererNode);
+		OnViewportSizeChanged();
 	}
 
 	public override void _Process(double delta)
